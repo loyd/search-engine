8159d0a617d3708c1e586dd996fc2c97cd21bb30
Paul Loyd <pavelko95@gmail.com>
2016-02-28 00:28:46 +0300

    feat: normalize link urls

--- src/extractor.js
+++ src/extractor.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import urllib from 'url';
+import punycode from 'punycode';
 
 import {Parser} from 'htmlparser2';
 import {Readability} from 'readabilitySAX';
@@ -46,6 +47,8 @@ class Handler extends Readability {
     return name && attribs[name];
   }
 }
+
+const reDupSlashes = /\/{2,}/g;
 
 class InfoCollector {
   constructor(urlFilter, ignoreNofollow, linkStemLimit) {
@@ -134,7 +137,7 @@ class InfoCollector {
   }
 
   prepareLink(link) {
-    let resolved = urllib.resolve(this.pageUrl, link.href);
+    let resolved = urllib.resolve(this.pageUrl, this.normalizeUrl(link.href));
     let urlObj = urllib.parse(resolved);
 
     let url = `${urlObj.protocol}//${urlObj.host}${urlObj.pathname}`;
@@ -176,6 +179,50 @@ class InfoCollector {
             break;
         }
     }
+  }
+
+  normalizeUrl(url) {
+    url = url.trim();
+
+    let shorthand = url.startsWith('//');
+    let urlObj = urllib.parse(shorthand ? `http:${url}` : url);
+
+    delete urlObj.host;
+    delete urlObj.query;
+    delete urlObj.hash;
+
+    // Remove default port.
+    if (urlObj.port) {
+      if (urlObj.protocol === 'http:' && +urlObj.port === 80)
+        delete urlObj.port;
+      else if (urlObj.protocol === 'https:' && +urlObj.port === 443)
+        delete urlObj.port;
+    }
+
+    // Replace duplicate slashes.
+    if (urlObj.pathname)
+      urlObj.pathname = urlObj.pathname.replace(reDupSlashes, '/');
+
+    // IDN to unicode and remove "www.".
+    if (urlObj.hostname) {
+      let hostname = punycode.toUnicode(urlObj.hostname);
+      if (hostname.startsWith('www.'))
+        hostname = hostname.slice(4);
+
+      urlObj.hostname = hostname;
+    }
+
+    url = urllib.format(urlObj);
+
+    // Remove ending "/".
+    if (url.endsWith('/'))
+      url = url.slice(0, -1);
+
+    // Restore relative protocol.
+    if (shorthand)
+      url = url.slice(5);
+
+    return url;
   }
 }
 
